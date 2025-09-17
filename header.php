bc47d6d954bdc98cb0db8df6b194ab7317f32ac5
Oleg Sh <dlyshona@yandex.ru>
2025-09-17 09:29:53 +0400

    trabslated into PHP

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104135411', 'ym');
+
+    ym(104135411, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/104135411" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+         <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2T07K7QJSK"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-2T07K7QJSK');
+        </script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=
+"Выбрать парикмахера, стрижки, причёски, парикмахерские, салоны красоты, выбрать, выбрать парикмахерскую, структура волос, строение волос, волосы, Как мыть волосы, как правильно мыть голову, как часто мыть волосы, моем голову" />
+<!-- 
+"Выбрать парикмахера, стрижки, причёски, парикмахерские, салоны красоты, выбрать, выбрать парикмахерскую" /> 
+Выбрать парикмахера
+-->
+
+<!-- 
+"структура волос, строение волос, волосы, стрижки, причёски, парикмахерские, салоны красоты, выбрать, выбрать парикмахерскую" /> 
+stroenie_volos
+-->
+<!-- 
+"Как мыть волосы, как правильно мыть голову, как часто мыть волосы, моем голову, парикмахерские, салоны красоты, выбрать, выбрать парикмахерскую" /> 
+ksk_myt
+-->
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"> -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Strigem!</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <nav class="nav">
+                <a href="index.php" class="logo">Strigem</a>
+                <ul class="menu">
+                    <li class="menu-item"><a href="index.php" class="menu-link">Как выбрать парикмахера</a></li>
+                    <li class="menu-item"><a href="stroenie_volos.php" class="menu-link">Структура волос</a></li>
+                    <li class="menu-item"><a href="kak_myt.php" class="menu-link">Как мыть</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
